dc02209b3774bfbb81944d5e7a90a17edb599ddd
agardelakos <agardelakos@gmail.com>
2019-12-04 23:56:48 +0100

    feature/docket_to_travis: changes function name to conform to pytest rules

--- tests/sample_test.py
+++ tests/sample_test.py
@@ -4,6 +4,6 @@ url = 'http://127.0.0.1:5000'
 docker_url = 'http://127.0.0.1:8000'
 
 
-def docker_test_api_workflows():
+def test_docker_api_work_flows():
     r = requests.get(docker_url + '/api/workflows')  # Assumes that it has a path of "/"
     assert r.status_code == 200  # Assumes that it will return a 200 response
