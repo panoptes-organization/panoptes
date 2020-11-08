5509e9b83c39dfe2d46c4b760f9c5cd119699606
agardelakos <agardelakos@gmail.com>
2020-11-08 18:01:26 +0000

    finished testing for all GET methods

--- panoptes/tests/api_test.py
+++ panoptes/tests/api_test.py
@@ -1,9 +1,81 @@
 import requests
+import panoptes.tests.api_test_helper as helper
 
-url = 'http://127.0.0.1:5000'
+local_url = 'http://127.0.0.1:5000'
 docker_url = 'http://127.0.0.1:8000'
 
+# CI server uses docker, to run locally use local_url
+url_to_use = local_url
+# Workflow and job entries to check. We don't check the date related entries
+workflow_entries = ['id', 'jobs_done', 'jobs_total']
+job_entries = ['input', 'is_checkpoint', 'jobid', 'log', 'msg', 'name', 'output', 'shell_command', 'status',
+               'wildcards', 'workflow_id']
 
-def test_docker_api_work_flows():
-    r = requests.get(docker_url + '/api/workflows')  # Assumes that it has a path of "/"
-    assert r.status_code == 200  # Assumes that it will return a 200 response
+
+#   Get all workflows
+def test_workflows():
+    response = requests.request("GET", url_to_use + '/api/workflows', headers=helper.headers)
+    assert response.status_code == 200
+    assert response.json()["count"] == 1
+    for entry in workflow_entries:
+        assert response.json()["workflows"][0][entry] == helper.workflows[0][entry]
+    helper.pretty_print_request(response.request)
+    helper.pretty_print_response(response)
+
+
+#   Non existent workflow
+def test_specific_work_flow_not_found():
+    response = requests.request("GET", url_to_use + '/api/workflow/100', headers=helper.headers)
+    assert response.status_code == 200
+    assert response.json()["msg"] == "Workflow not found"
+    helper.pretty_print_request(response.request)
+    helper.pretty_print_response(response)
+
+
+#   Valid workflow
+def test_specific_work_flow_valid():
+    response = requests.request("GET", url_to_use + '/api/workflow/1', headers=helper.headers)
+    assert response.status_code == 200
+    for entry in workflow_entries:
+        assert response.json()["workflow"][entry] == helper.workflows[0][entry]
+    helper.pretty_print_request(response.request)
+    helper.pretty_print_response(response)
+
+
+#   All jobs for a valid workflow
+def test_specific_work_flow_all_jobs_valid():
+    response = requests.request("GET", url_to_use + '/api/workflow/1/jobs', headers=helper.headers)
+    assert response.status_code == 200
+    assert response.json()["count"] == 14
+    for entry in job_entries:
+        assert response.json()["jobs"][0][entry] == helper.jobs[entry]
+    helper.pretty_print_request(response.request)
+    helper.pretty_print_response(response)
+
+
+#   Jobs for non existent workflow
+def test_specific_work_flow_jobs_not_found():
+    response = requests.request("GET", url_to_use + '/api/workflow/100/jobs', headers=helper.headers)
+    assert response.status_code == 200
+    assert response.json()["msg"] == "Workflow not found"
+    helper.pretty_print_request(response.request)
+    helper.pretty_print_response(response)
+
+
+#   Specific job for a specific workflow
+def test_specific_work_flow_and_job_valid():
+    response = requests.request("GET", url_to_use + '/api/workflow/1/job/3', headers=helper.headers)
+    assert response.status_code == 200
+    for entry in job_entries:
+        assert response.json()["jobs"][entry] == helper.jobs[entry]
+    helper.pretty_print_request(response.request)
+    helper.pretty_print_response(response)
+
+
+#   Non existent job for a specific workflow
+def test_specific_work_flow_and_job_non_existent():
+    response = requests.request("GET", url_to_use + '/api/workflow/1/job/100', headers=helper.headers)
+    assert response.status_code == 200
+    assert response.json()["msg"] == "Job not found"
+    helper.pretty_print_request(response.request)
+    helper.pretty_print_response(response)
