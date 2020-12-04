24153b3daf1f78600b7ff03e804da3b6a4fc8282
drekoumis <mitsatsos13@gmail.com>
2020-12-04 16:43:22 +0200

    fix and creat api test for delete,rename,db clean up

--- panoptes/tests/api_test.py
+++ panoptes/tests/api_test.py
@@ -14,19 +14,22 @@ job_entries = ['input', 'is_checkpoint', 'jobid', 'log', 'msg', 'name', 'output'
 
 #   Get all workflows
 def test_workflows():
-    response = requests.request("GET", url_to_use + '/api/workflows', headers=helper.headers)
+    response = requests.request(
+        "GET", url_to_use + '/api/workflows', headers=helper.headers)
     assert response.status_code == 200
-    assert response.json()["count"] == 1
+    assert response.json()["count"] == 2
     for entry in workflow_entries:
-        assert response.json()["workflows"][0][entry] == helper.workflows[0][entry]
+        assert response.json()[
+            "workflows"][0][entry] == helper.workflows[0][entry]
     helper.pretty_print_request(response.request)
     helper.pretty_print_response(response)
 
 
 #   Non existent workflow
 def test_specific_work_flow_not_found():
-    response = requests.request("GET", url_to_use + '/api/workflow/100', headers=helper.headers)
-    assert response.status_code == 200
+    response = requests.request(
+        "GET", url_to_use + '/api/workflow/100', headers=helper.headers)
+    assert response.status_code == 404
     assert response.json()["msg"] == "Workflow not found"
     helper.pretty_print_request(response.request)
     helper.pretty_print_response(response)
@@ -34,7 +37,8 @@ def test_specific_work_flow_not_found():
 
 #   Valid workflow
 def test_specific_work_flow_valid():
-    response = requests.request("GET", url_to_use + '/api/workflow/1', headers=helper.headers)
+    response = requests.request(
+        "GET", url_to_use + '/api/workflow/1', headers=helper.headers)
     assert response.status_code == 200
     for entry in workflow_entries:
         assert response.json()["workflow"][entry] == helper.workflows[0][entry]
@@ -44,7 +48,8 @@ def test_specific_work_flow_valid():
 
 #   All jobs for a valid workflow
 def test_specific_work_flow_all_jobs_valid():
-    response = requests.request("GET", url_to_use + '/api/workflow/1/jobs', headers=helper.headers)
+    response = requests.request(
+        "GET", url_to_use + '/api/workflow/1/jobs', headers=helper.headers)
     assert response.status_code == 200
     assert response.json()["count"] == 14
     for entry in job_entries:
@@ -55,8 +60,9 @@ def test_specific_work_flow_all_jobs_valid():
 
 #   Jobs for non existent workflow
 def test_specific_work_flow_jobs_not_found():
-    response = requests.request("GET", url_to_use + '/api/workflow/100/jobs', headers=helper.headers)
-    assert response.status_code == 200
+    response = requests.request(
+        "GET", url_to_use + '/api/workflow/100/jobs', headers=helper.headers)
+    assert response.status_code == 404
     assert response.json()["msg"] == "Workflow not found"
     helper.pretty_print_request(response.request)
     helper.pretty_print_response(response)
@@ -64,7 +70,8 @@ def test_specific_work_flow_jobs_not_found():
 
 #   Specific job for a specific workflow
 def test_specific_work_flow_and_job_valid():
-    response = requests.request("GET", url_to_use + '/api/workflow/1/job/3', headers=helper.headers)
+    response = requests.request(
+        "GET", url_to_use + '/api/workflow/1/job/3', headers=helper.headers)
     assert response.status_code == 200
     for entry in job_entries:
         assert response.json()["jobs"][entry] == helper.jobs[entry]
@@ -74,8 +81,127 @@ def test_specific_work_flow_and_job_valid():
 
 #   Non existent job for a specific workflow
 def test_specific_work_flow_and_job_non_existent():
-    response = requests.request("GET", url_to_use + '/api/workflow/1/job/100', headers=helper.headers)
-    assert response.status_code == 200
+    response = requests.request(
+        "GET", url_to_use + '/api/workflow/1/job/100', headers=helper.headers)
+    assert response.status_code == 404
     assert response.json()["msg"] == "Job not found"
+    helper.pretty_print_request(response.request)
+    helper.pretty_print_response(response)
+
+
+#   Non existent workflow and job for a specific workflow
+def test_specific_work_flow_job_non_existent():
+    response = requests.request(
+        "GET", url_to_use + '/api/workflow/100/job/1', headers=helper.headers)
+    assert response.status_code == 404
+    assert response.json()["msg"] == "Workflow not found"
+    helper.pretty_print_request(response.request)
+    helper.pretty_print_response(response)
+
+
+#   Specific rename a specific workflow
+def test_specific_work_flow_re_name_existent():
+    response_old = requests.request(
+        "GET", url_to_use + '/api/workflow-rename/1/old_name', headers=helper.headers)
+    response = requests.request(
+        "GET", url_to_use + '/api/workflow-rename/1/new_name', headers=helper.headers)
+    assert response.status_code == 200
+    assert response.json()[
+        "msg"] == "The name change from old_name to new_name"
+    helper.pretty_print_request(response.request)
+    helper.pretty_print_response(response)
+
+
+#   Non existent specific rename a specific workflow
+def test_specific_work_flow_re_name_non_existent():
+    response = requests.request(
+        "GET", url_to_use + '/api/workflow-rename/100/new_name', headers=helper.headers)
+    assert response.status_code == 404
+    assert response.json()["msg"] == "Workflow not found"
+    helper.pretty_print_request(response.request)
+    helper.pretty_print_response(response)
+
+
+#   Specific workflow rename a specific job
+def test_specific_work_flow_re_name_job_existent():
+    response_old = requests.request(
+        "GET", url_to_use + '/api/workflow-rename/1/job/3/old_name', headers=helper.headers)
+    response = requests.request(
+        "GET", url_to_use + '/api/workflow-rename/1/job/3/new_name', headers=helper.headers)
+    assert response.status_code == 200
+    assert response.json()[
+        "msg"] == "The name change from old_name to new_name"
+    helper.pretty_print_request(response.request)
+    helper.pretty_print_response(response)
+
+
+#   Non existent specific job rename at specific existent workflow
+def test_specific_work_flow_re_name_job_non_existent():
+    response = requests.request(
+        "GET", url_to_use + '/api/workflow-rename/1/job/100/new_name', headers=helper.headers)
+    assert response.status_code == 404
+    assert response.json()["msg"] == "Job not found"
+    helper.pretty_print_request(response.request)
+    helper.pretty_print_response(response)
+
+
+#   specific job rename at specific non existent workflow
+def test_specific_non_existent_work_flow_re_name_job():
+    response = requests.request(
+        "GET", url_to_use + '/api/workflow-rename/100/job/3/new_name', headers=helper.headers)
+    assert response.status_code == 404
+    assert response.json()["msg"] == "Workflow not found"
+    helper.pretty_print_request(response.request)
+    helper.pretty_print_response(response)
+
+
+#   Specific workflow delete existent
+def test_specific_work_flow_delete_existent():
+    response = requests.request(
+        "GET", url_to_use + '/api/delete/1', headers=helper.headers)
+    assert response.status_code == 200
+    assert response.json()["msg"] == "Delete Complete Correctly "
+    helper.pretty_print_request(response.request)
+    helper.pretty_print_response(response)
+
+
+#   Specific workflow delete a specific status Running
+def test_specific_work_flow_running_delete_existent():
+    response = requests.request(
+        "GET", url_to_use + '/api/delete/2', headers=helper.headers)
+    assert response.status_code == 403
+    assert response.json()["msg"] == "You cannot delete Running Workflow "
+    helper.pretty_print_request(response.request)
+    helper.pretty_print_response(response)
+
+#   Specific workflow delete non existent
+
+
+def test_specific_work_flow_delete_non_existent():
+    response = requests.request(
+        "GET", url_to_use + '/api/delete/100', headers=helper.headers)
+    assert response.status_code == 404
+    assert response.json()[
+        "msg"] == "Unable to delete Workflow 100. Please check if workflow 100 exists."
+    helper.pretty_print_request(response.request)
+    helper.pretty_print_response(response)
+
+
+#   clean up the whole database
+def test_clean_up_database_existent():
+    response = requests.request(
+        "GET", url_to_use + '/api/clean-up-database', headers=helper.headers)
+    assert response.status_code == 200
+    assert response.json()["msg"] == "Database clean up is complete"
+    helper.pretty_print_request(response.request)
+    helper.pretty_print_response(response)
+
+
+#   clean up the whole database non existent
+def test_clean_up_database_non_existent():
+    response = requests.request(
+        "GET", url_to_use + '/api/clean-up-database', headers=helper.headers)
+    assert response.status_code == 510
+    assert response.json()["msg"] == "Database is empty"
     helper.pretty_print_request(response.request)
     helper.pretty_print_response(response)
