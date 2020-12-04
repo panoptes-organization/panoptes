56fe5d89e4968b1a8420be65444130a0985cebe2
drekoumis <mitsatsos13@gmail.com>
2020-12-05 01:23:35 +0200

    add 2 wf at first test

--- panoptes/tests/api_test_helper.py
+++ panoptes/tests/api_test_helper.py
@@ -14,12 +14,6 @@ workflows = [{
     'jobs_done': 14,
     'jobs_total': 14,
     'status': "Done"
-    },
-    {
-    'id': 2,
-    'jobs_done': 13,
-    'jobs_total': 14,
-    'status': "Running"
 }]
 
 jobs = {
