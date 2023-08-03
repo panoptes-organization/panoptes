27abd2d5e49b3119959f14679cdd4f52cb659354
Gypas, Foivos <foivos.gypas@novartis.com>
2023-08-03 16:09:42 +0200

    Release 0.2.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if sys.version_info < (3, 6):
 
 setup(
     name='panoptes-ui',
-    version='0.2.1',
+    version='0.2.2',
     url='https://github.com/panoptes-organization/panoptes',
     license='MIT',
     author='panoptes-organization',
